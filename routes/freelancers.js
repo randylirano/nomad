d5b48a31cd9d1c9facca3f1e7bdfe599fb487ea7
kezumah <kennedy.ezumah@gmail.com>
2021-11-07 17:10:19 -0800

    Add files via upload

--- routes/freelancers.js
+++ routes/freelancers.js
@@ -1,63 +1,27 @@
 let express = require("express");
 let router = express.Router();
 
-/* GET listings */
-// simulation db
-const freelancersStub = [
-  {
-    name:"Randy Lirano",
-    email:"lirano.randy@fakemail.com",
-    phone:"+1 (111) 111-1111",
-    skills:["Python", "Java"],
-    links:""
-  },
-  {
-    name:"Kennedy Ezumah",
-    email:"ezumah.kennedy@fakemail.com",
-    phone:"+1 (222) 222-2222",
-    skills:"Python",
-    links:""
-  },
-  {
-    name:"Paola Quevedo",
-    email:"quevedo.paola@fakemail.com",
-    phone:"+1 (333) 333-3333",
-    skills:"Python",
-    links:""
-  },
-  {
-    name:"John Alexis",
-    email:"alexis.john@fakemail.com",
-    phone:"+1 (444) 444-4444",
-    skills:"Python",
-    links:""
-  },
-  {
-    name:"John Guerra",
-    email:"guerra.john@fakemail.com",
-    phone:"+1 (555) 555-5555",
-    skills:"Python",
-    links:""
-  },
-];
+const nomadDB = require("../db/nomadDB.js");
 
-router.get("/", function (req, res) {
-  res.json(freelancersStub);
+/* GET listings */
+// get all freelancers information
+router.get("/", async function (req, res) {
+  const freelancers = await nomadDB.getFreelancers();
+
+  res.json(freelancers);
 });
 
-router.post("/create", function(req, res) {
-  const freelancer = req.body;
-  console.log("create freelancer", freelancer);
+router.post("/create", async function (req, res) {
+  const newFreelancer = req.body;
+  
+  try {
+    const postRes = await nomadDB.createFreelancer(newFreelancer);
+    console.log("New freelancer created", postRes);
+  } catch (err) {
+    console.log("FAILED INSERTING", err);
+  }
 
-  freelancersStub.push({
-    name:freelancer.name,
-    email:freelancer.email,
-    phone:freelancer.phone,
-    skills:freelancer.skills,
-    links:freelancer.links
-  });
-
-  res.redirect("/");
+  res.json({"status":"OK"});
 });
 
 module.exports = router;
