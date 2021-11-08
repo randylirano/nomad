ce1baf2e5da1083cf59a1186f64d4738023e5852
kezumah <kennedy.ezumah@gmail.com>
2021-11-07 17:09:45 -0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,51 @@
 let express = require("express");
 let router = express.Router();
-const myDB = require("../db/myMongoDB.js");
 
-console.log("users.js: module loaded...");
+// import module to interact with DB
+const nomadDB = require("../db/myMongoDB.js");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+// routes to create new user during sign-up
+router.post("/create", async (req, res) => {
+  const body = req.body;
+
+  try {
+    const checkCredential = await nomadDB.getCredential(body);
+
+    if (checkCredential.length == 0) {
+      console.log("NO DUPLICATE FOUND");
+      const createRes = await nomadDB.createUser(body);
+      console.log("User created", createRes);
+    } else {
+      console.log("DUPLICATE FOUND");
+    }
+  } catch (err) {
+    console.log("FAILED INSERTING", err);
+  }
+
+  res.json({"status":"OK"});
 });
 
 /* Subroute used for verifying user login */
 router.post("/query", async function (req, res) {
   console.log("users.js: '/query' route invoked...");
   console.log(req.body);
-  // define query from get request
-  let query = {};
-  query.login_email = req.body.login_email;
-  query.password = req.body.password;
+  const body = req.body;
 
-  // Call MongoDB server
-  const matchingUsers = await myDB.authenticateUsers(query);
-  console.log(matchingUsers);
+  try {
+    const checkCredential = await nomadDB.getCredential(body);
 
-  // check that an empty list was not returned, meaning no matches to the query
-  // redirection will be handled by client-side JS
-  if (matchingUsers.length == 0) {
-    res.sendStatus(404);
+    if (checkCredential.length == 0) {
+      console.log("NO DUPLICATE FOUND");
+      const createRes = await nomadDB.createUser(body);
+
+    } else {
+      console.log("DUPLICATE FOUND");
+    }
+  } catch (err) {
+    console.log("FAILED INSERTING", err);
   }
 
-  // otherwise, authentication was successful and now redirect to projects page
-  // redirection will be handled by client-side JS
-  else {
-    res.sendStatus(200);
-  }
+  res.json({"status":"OK"});
 });
 
 module.exports = router;
